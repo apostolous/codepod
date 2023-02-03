1fb3c4924d624a3aaf382e4ee122f7a8136f7140
SpBills <spencebills0@gmail.com>
2023-02-03 00:25:02 +0000

    added readme stuff

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ CodePods is an easy way to manage your development environments
 This was developed originally for a team of part-time developers that constantly switch projects.
 
 This was inspired by [DevPods at Uber](https://www.uber.com/blog/devpod-improving-developer-productivity-at-uber/). 
+
+Eventually, this should be migrated to using Bazel as a monorepo manager.
--- x.py
+++ x.py
@@ -1,7 +1,37 @@
 import argparse
 
+def compile(ver):
+  print("compiling " + ver())
+
+def admin():
+  return "adm"
+
+def client():
+  return "cli"
+
+def server():
+  return "serv"
+
 def main():
   print("CodePods Compiler Service")
+
+  parser = argparse.ArgumentParser(prog="CodePods")
+
+  parser.add_argument("-a", "--admin", action="store_true", required=False)
+  parser.add_argument("-c", "--client", action="store_true", required=False)
+  parser.add_argument("-s", "--server", action="store_true", required=False)
+
+  args = parser.parse_args();
+
+  if args.admin:
+    compile(admin)
+  
+  if args.client:
+    compile(client)
+
+  if args.server:
+    compile(server)
+
 
 if __name__ == "__main__":
   main()
